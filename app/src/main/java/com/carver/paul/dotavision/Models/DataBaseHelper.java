9a44b3a6de8f681f286e246c63b70d16389c0d5c
Paul Broadbent <dbdkmezz@gmail.com>
2016-03-20 08:40:18 +0000

    updated database

--- app/src/main/java/com/carver/paul/dotavision/Models/DataBaseHelper.java
+++ app/src/main/java/com/carver/paul/dotavision/Models/DataBaseHelper.java
@@ -7,7 +7,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class DataBaseHelper extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "advantages.db";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
 
     public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,59 +23,4 @@ public class DataBaseHelper extends SQLiteAssetHelper {
         //super(context, DATABASE_NAME, context.getExternalFilesDir(null).getAbsolutePath(), null, DATABASE_VERSION);
 
     }
-
-/*    public Cursor getEmployees() {
-
-        SQLiteDatabase db = getReadableDatabase();
-        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-
-        String [] sqlSelect = {"0 _id", "FirstName", "LastName"};
-        String sqlTables = "Employees";
-
-        qb.setTables(sqlTables);
-        Cursor c = qb.query(db, sqlSelect, null, null,
-                null, null, null);
-
-        c.moveToFirst();
-        return c;
-
-    }*/
-
 }
-
-/*
-package com.carver.paul.dotavision.Models;
-
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteQueryBuilder;
-
-import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
-
-public class DataBaseHelper extends SQLiteAssetHelper {
-
-    private static final String DATABASE_NAME = "advantages.db";
-    private static final int DATABASE_VERSION = 1;
-
-    public DataBaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    public Cursor getHeroes() {
-
-        SQLiteDatabase db = getReadableDatabase();
-        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-
-        String [] sqlSelect = {"_id", "name"};
-        String sqlTables = "Heroes";
-
-        qb.setTables(sqlTables);
-        Cursor c = qb.query(db, sqlSelect, null, null,
-                null, null, null);
-
-        c.moveToFirst();
-        return c;
-
-    }
-}*/
