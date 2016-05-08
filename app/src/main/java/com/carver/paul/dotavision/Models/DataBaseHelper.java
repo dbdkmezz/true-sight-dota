67f953c66bc87501f302565a481af8b1cc327f78
Paul Broadbent <dbdkmezz@gmail.com>
2016-05-08 18:52:11 +0100

    merge fix

--- app/src/main/java/com/carver/paul/dotavision/Models/DataBaseHelper.java
+++ app/src/main/java/com/carver/paul/dotavision/Models/DataBaseHelper.java
@@ -7,7 +7,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class DataBaseHelper extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "advantages.db";
-    private static final int DATABASE_VERSION = 6;
+    private static final int DATABASE_VERSION = 7;
 
     public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
