dd3959f63fd89967780ad6605f05a764e0565728
Paul Broadbent <dbdkmezz@gmail.com>
2016-10-15 17:56:52 +0100

    removed dead file

--- app/src/main/java/com/carver/paul/dotavision/RecognitionPresenter.java
+++
@@ -1,7 +0,0 @@
-/*
-
-This file has moved. It is now located at:
-
-https://github.com/dbdkmezz/true-sight-dota/blob/master/app/src/main/java/com/carver/paul/dotavision/Models/DataManager.java
-
-*/
