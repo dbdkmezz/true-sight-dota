65dedbe861eb172d7f17ed0ea7a5d2edd9b77c4f
Paul Broadbent <dbdkmezz@gmail.com>
2016-10-15 11:11:10 +0100

    disabled robolectric test

--- app/src/test/java/com/carver/paul/dotavision/CameraActivityTest.java
+++ app/src/test/java/com/carver/paul/dotavision/CameraActivityTest.java
@@ -1,73 +1,73 @@
-package com.carver.paul.dotavision;
-
-import android.view.View;
-import android.widget.ImageButton;
-
-import com.carver.paul.dotavision.Ui.DotaCamera.CameraActivity;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-//TODO-beauty: add test for taking pictures (will have to stop the code needing to write a file)
-
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21,
-        shadows = {CustomShadowParameters.class, CustomShadowCamera.class})
-public class CameraActivityTest {
-
-    private CameraActivity activity;
-
-    @Before
-    public void setUp() throws Exception
-    {
-        activity = Robolectric.buildActivity(CameraActivity.class)
-                .create()
-                .resume()
-                .get();
-    }
-
-    @Test
-    public void shouldNotBeNull() throws Exception {
-        assertNotNull(activity);
-    }
-
-    @Test
-    public void shouldHaveCaptureButton() {
-        ImageButton capture = (ImageButton) activity.findViewById(R.id.button_capture);
-        assertNotNull(capture);
-        assertTrue(capture.getVisibility() == View.VISIBLE);
-    }
-
-    @Test
-    public void shouldHaveInvisibleConfirmButton() {
-        ImageButton confirm = (ImageButton) activity.findViewById(R.id.button_confirm);
-        assertNotNull(confirm);
-        assertTrue(confirm.getVisibility() != View.VISIBLE);
-    }
-
-    @Test
-    public void shouldHaveInvisibleTakeAgainButton() {
-        ImageButton takeAgain = (ImageButton) activity.findViewById(R.id.button_take_again);
-        assertNotNull(takeAgain);
-        assertTrue(takeAgain.getVisibility() != View.VISIBLE);
-    }
-
-/*    @Test
-    public void clickingCapture_shouldShowOtherButtons() {
-        Robolectric.flushBackgroundThreadScheduler();
-        activity.findViewById(R.id.button_capture).performClick();
-
-        Robolectric.flushBackgroundThreadScheduler();
-
-        assertTrue(activity.findViewById(R.id.button_capture).getVisibility() != View.VISIBLE);
-        assertTrue(activity.findViewById(R.id.button_take_again).getVisibility() == View.VISIBLE);
-        assertTrue(activity.findViewById(R.id.button_confirm).getVisibility() == View.VISIBLE);
-    }*/
-}
+//package com.carver.paul.dotavision;
+//
+//import android.view.View;
+//import android.widget.ImageButton;
+//
+//import com.carver.paul.dotavision.Ui.DotaCamera.CameraActivity;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.robolectric.Robolectric;
+//import org.robolectric.RobolectricGradleTestRunner;
+//import org.robolectric.annotation.Config;
+//
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertTrue;
+//
+////TODO-beauty: add test for taking pictures (will have to stop the code needing to write a file)
+//
+//@RunWith(RobolectricGradleTestRunner.class)
+//@Config(constants = BuildConfig.class, sdk = 21,
+//        shadows = {CustomShadowParameters.class, CustomShadowCamera.class})
+//public class CameraActivityTest {
+//
+//    private CameraActivity activity;
+//
+//    @Before
+//    public void setUp() throws Exception
+//    {
+//        activity = Robolectric.buildActivity(CameraActivity.class)
+//                .create()
+//                .resume()
+//                .get();
+//    }
+//
+//    @Test
+//    public void shouldNotBeNull() throws Exception {
+//        assertNotNull(activity);
+//    }
+//
+//    @Test
+//    public void shouldHaveCaptureButton() {
+//        ImageButton capture = (ImageButton) activity.findViewById(R.id.button_capture);
+//        assertNotNull(capture);
+//        assertTrue(capture.getVisibility() == View.VISIBLE);
+//    }
+//
+//    @Test
+//    public void shouldHaveInvisibleConfirmButton() {
+//        ImageButton confirm = (ImageButton) activity.findViewById(R.id.button_confirm);
+//        assertNotNull(confirm);
+//        assertTrue(confirm.getVisibility() != View.VISIBLE);
+//    }
+//
+//    @Test
+//    public void shouldHaveInvisibleTakeAgainButton() {
+//        ImageButton takeAgain = (ImageButton) activity.findViewById(R.id.button_take_again);
+//        assertNotNull(takeAgain);
+//        assertTrue(takeAgain.getVisibility() != View.VISIBLE);
+//    }
+//
+///*    @Test
+//    public void clickingCapture_shouldShowOtherButtons() {
+//        Robolectric.flushBackgroundThreadScheduler();
+//        activity.findViewById(R.id.button_capture).performClick();
+//
+//        Robolectric.flushBackgroundThreadScheduler();
+//
+//        assertTrue(activity.findViewById(R.id.button_capture).getVisibility() != View.VISIBLE);
+//        assertTrue(activity.findViewById(R.id.button_take_again).getVisibility() == View.VISIBLE);
+//        assertTrue(activity.findViewById(R.id.button_confirm).getVisibility() == View.VISIBLE);
+//    }*/
+//}
--- app/src/test/java/com/carver/paul/dotavision/MainActivityTest.java
+++ app/src/test/java/com/carver/paul/dotavision/MainActivityTest.java
@@ -1,74 +1,74 @@
-package com.carver.paul.dotavision;
-
-import android.support.design.widget.NavigationView;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.carver.paul.dotavision.Ui.MainActivity;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-//TODO-beauty: add test for the main activity to click the demo button. Will need to re-write some
-// of the image processing code to make it suitable for testing.
-
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
-public class MainActivityTest {
-
-    private MainActivity activity;
-
-    @Before
-    public void setUp() throws Exception
-    {
-        activity = Robolectric.buildActivity(MainActivity.class )
-                .create()
-                .resume()
-                .get();
-    }
-
-    @Test
-    public void shouldNotBeNull() {
-        assertNotNull(activity);
-    }
-
-    @Test
-    public void shouldHaveToobarAndNavView() {
-        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
-        assertNotNull(toolbar);
-        assertTrue(toolbar.getVisibility() == View.VISIBLE);
-
-        NavigationView navView = (NavigationView) activity.findViewById(R.id.nav_view);
-        assertNotNull(navView);
-        assertTrue(navView.getVisibility() == View.VISIBLE);
-    }
-
-    @Test
-    public void shouldHaveTopImage() {
-        ImageView topImage = (ImageView) activity.findViewById(R.id.image_top);
-        assertNotNull(topImage);
-    }
-
-    @Test
-    public void hiddenViewsNotVisible() {
-        TextView processingText = (TextView) activity.findViewById(R.id.text_processing_image);
-        assertNotNull(processingText);
-        assertTrue(processingText.getVisibility() != View.VISIBLE);
-    }
-/*
-    @Test
-    public void clickingTakePhoto_shouldStartCameraActivity() {
-        activity.findViewById(R.id.button_fab_take_photo).performClick();
-        Intent expectedIntent = new Intent(activity, CameraActivity.class);
-        assertTrue(shadowOf(activity).getNextStartedActivity().equals(expectedIntent));
-    }*/
-}
+//package com.carver.paul.dotavision;
+//
+//import android.support.design.widget.NavigationView;
+//import android.support.v7.widget.Toolbar;
+//import android.view.View;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import com.carver.paul.dotavision.Ui.MainActivity;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.robolectric.Robolectric;
+//import org.robolectric.RobolectricGradleTestRunner;
+//import org.robolectric.annotation.Config;
+//
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertTrue;
+//
+////TODO-beauty: add test for the main activity to click the demo button. Will need to re-write some
+//// of the image processing code to make it suitable for testing.
+//
+//@RunWith(RobolectricGradleTestRunner.class)
+//@Config(constants = BuildConfig.class, sdk = 21)
+//public class MainActivityTest {
+//
+//    private MainActivity activity;
+//
+//    @Before
+//    public void setUp() throws Exception
+//    {
+//        activity = Robolectric.buildActivity(MainActivity.class )
+//                .create()
+//                .resume()
+//                .get();
+//    }
+//
+//    @Test
+//    public void shouldNotBeNull() {
+//        assertNotNull(activity);
+//    }
+//
+//    @Test
+//    public void shouldHaveToobarAndNavView() {
+//        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+//        assertNotNull(toolbar);
+//        assertTrue(toolbar.getVisibility() == View.VISIBLE);
+//
+//        NavigationView navView = (NavigationView) activity.findViewById(R.id.nav_view);
+//        assertNotNull(navView);
+//        assertTrue(navView.getVisibility() == View.VISIBLE);
+//    }
+//
+//    @Test
+//    public void shouldHaveTopImage() {
+//        ImageView topImage = (ImageView) activity.findViewById(R.id.image_top);
+//        assertNotNull(topImage);
+//    }
+//
+//    @Test
+//    public void hiddenViewsNotVisible() {
+//        TextView processingText = (TextView) activity.findViewById(R.id.text_processing_image);
+//        assertNotNull(processingText);
+//        assertTrue(processingText.getVisibility() != View.VISIBLE);
+//    }
+///*
+//    @Test
+//    public void clickingTakePhoto_shouldStartCameraActivity() {
+//        activity.findViewById(R.id.button_fab_take_photo).performClick();
+//        Intent expectedIntent = new Intent(activity, CameraActivity.class);
+//        assertTrue(shadowOf(activity).getNextStartedActivity().equals(expectedIntent));
+//    }*/
+//}
