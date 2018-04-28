c47cd51c26fac8841de2f82abbab08d41dda76be
Paul Carver <dbdkmezz@gmail.com>
2018-04-28 17:19:55 +0100

    fix advantages

--- app/src/main/java/com/carver/paul/truesight/Models/DataBaseHelper.java
+++ app/src/main/java/com/carver/paul/truesight/Models/DataBaseHelper.java
@@ -7,7 +7,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class DataBaseHelper extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "advantages.db";
-    private static final int DATABASE_VERSION = 27;
+    private static final int DATABASE_VERSION = 28;
 
     public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
